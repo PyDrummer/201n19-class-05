c950f3aa9a59971f17e4034cc9375163df5e97e0
Anthony Beaver <anthonybeavermusic@gmail.com>
2020-08-01 14:32:12 -0700

    problem four solved

--- starter-code/app.js
+++ starter-code/app.js
@@ -61,7 +61,7 @@ function sumAndMultiply(a, b, c) { //eslint-disable-line
   var stringOne = `${a} and ${b} and ${c} sum to ${sumAnswerTwo[0]}.`;
   var stringTwo = `The product of ${a} and ${b} and ${c} is ${multiAnswerTwo[0]}.`;
   var arrayAnswer = [sumAnswerTwo[0], multiAnswerTwo[0], stringOne, stringTwo];
-  console.log(arrayAnswer);
+  //console.log(arrayAnswer);
   return arrayAnswer;
 }
 
@@ -84,12 +84,16 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+  var answerOne = sum(sumArr[2], sum(sumArr[0], sumArr[1])[0])[0];
+  //var answerTwo = sum(answerOne, sumArr[2])[0];
+  //console.log(answerOne);
+  var string = `${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${answerOne} is their sum.`;
+  return [answerOne, string];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
